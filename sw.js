da99c58cc4c58fd99d52db58662846fbda1ab48a
bookkeralahouseboats <bookkeralahouseboats@gmail.com>
2026-07-28 16:53:23 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'houseboat-pwa-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  // Let Google Apps Script requests pass through directly to the network
+  if (event.request.url.includes('script.google.com')) {
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
